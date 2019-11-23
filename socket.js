d12b8a25976da99bada1d7668bbf72f4f7666665
Mathew Nuval <nuval.mathewj@gmail.com>
2019-11-23 14:10:42 -0800

    Implemented chat system :poop:

--- socket.js
+++ socket.js
@@ -1,13 +1,24 @@
-let app = require('http').createServer(); //create HTTP server
-let io = require('socket.io')(app, { path: '/socket.io' }); // bind Socket to HTTP server
-app.listen(3000); // listen on port 3000
-console.log('Listening for connection on port 3000');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', function (socket) {
-    console.log('Socket connected');
-    socket.emit('fromServer', { id: 'foo' }); //send message fromServer to client
-
-    socket.on('fromClient', function (data) {
-        console.log('Recieved ' + data.id + ' from client');
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
     });
+});
 
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
